4859553c4efe3b94ee0c19a14bab700eef390136
Ars <asd>
2023-04-28 14:09:55 +0300

    Fix

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-import {getRandomInt} from './util';
+import {getRandomInt} from './util.js';
 
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel turpis molestie, tempor ligula et, sodales diam. Morbi suscipit facilisis ex, sit amet consequat ante. Donec ultricies urna arcu, eu egestas diam rhoncus ac. Phasellus at purus in libero pretium imperdiet. Phasellus tortor turpis, dapibus eu tortor vel, feugiat accumsan turpis. Aliquam erat volutpat. Cras at ex nisl. Morbi aliquet est dolor, sit amet ultricies mauris imperdiet sagittis. Suspendisse venenatis consectetur vulputate.';
 const loremIpsumList = loremIpsum.split('.');
