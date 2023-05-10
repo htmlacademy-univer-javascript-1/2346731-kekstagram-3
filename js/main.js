115438e154681d6ffed976f6bbeee2ec4a585253
Ars <asd>
2023-05-10 17:49:09 +0300

    Features: Image scaling and filters applying. Also added new fields to validation.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 import {showRandomPosts} from './randomPostsFactory.js';
-import {animateOpeningAndClosingOfUploadForm} from './upload_form/upload_form_animation.js';
+import {
+  animateImageScalingAndFiltersApplying,
+  animateOpeningAndClosingOfUploadForm
+} from './upload_form/upload_form_animation.js';
 import {activatePristineValidationOfUploadForm} from './upload_form/upload_form_validation.js';
 
 showRandomPosts();
 animateOpeningAndClosingOfUploadForm();
 activatePristineValidationOfUploadForm();
+animateImageScalingAndFiltersApplying();
--- js/upload_form/upload_form_validation.js
+++ js/upload_form/upload_form_validation.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#upload-select-image');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
+const scaleValueElement = document.querySelector('.scale__control--value');
 const pristine = new Pristine(form, {
   classTo: 'form__item',
   errorClass: 'form__item--invalid',
@@ -12,6 +13,7 @@ const pristine = new Pristine(form, {
 const hashtagRegexp = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
 export function activatePristineValidationOfUploadForm() {
+  pristine.addValidator(scaleValueElement, validateScaleValueElement, 'Неправильно выбран масштаб изображения. Допустимые значения: 25%, 50%, 75%, 100%');
   pristine.addValidator(commentInput, validateComment, 'Количество символов в комментарии должно быть от 20 до 140');
   pristine.addValidator(hashtagInput, validateHashtag, 'Хештег должен содержать от 1 до 19 символов и начинаться с решётки');
   form.addEventListener('submit', (evt) => {
@@ -20,7 +22,11 @@ export function activatePristineValidationOfUploadForm() {
       form.submit();
     }
   });
+}
 
+function validateScaleValueElement() {
+  const value = scaleValueElement.value;
+  return value === '25%' || value === '50%' || value === '75%' || value === '100%';
 }
 
 function validateComment (value) {
